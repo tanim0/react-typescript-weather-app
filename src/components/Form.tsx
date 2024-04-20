43f3613f7bfd23c7b706e5a85c2da096ac081921
tanim0 <nyokki0731@gmail.com>
2024-04-20 15:40:55 +0900

    ボタンクリックでAPI取得

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 const Form = () => {
     const [city, setCity] = useState<string>("");
+    
+    // TypeScriptを使う場合はeにも型設定が必要。(型がわからないとき = any)
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=b1b0285f95c743d6b8862430242004&q=London&aqi=no")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
 
     return (
         <form>
@@ -11,7 +19,7 @@ const Form = () => {
                 placeholder="都市名"
                 onChange={e => setCity(e.target.value)}
             />
-            <button type="submit">
+            <button type="submit" onClick={getWeather}>
                 Get Weather
             </button>
         </form>
