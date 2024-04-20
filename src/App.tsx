f4ad65a0daa054d6ef23df34282f168cb3e3475b
tanim0 <nyokki0731@gmail.com>
2024-04-20 19:56:25 +0900

    apiデータ加工・表示

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,40 @@ import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
 
+type ResultsState =  {
+  country: string
+  cityName: string
+  temperature: string
+  conditionText: string
+  icon: string
+}
+
 const App = () => {
+  // satate・・・データを保管する場所
   const [city, setCity] = useState<string>("");
+  
+  const [results, setResults] = useState<ResultsState>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  });
 
   // TypeScriptを使う場合はeにも型設定が必要。(型がわからないとき = any)
   const getWeather = (e: any) => {
     e.preventDefault();
-    fetch("http://api.weatherapi.com/v1/current.json?key=b1b0285f95c743d6b8862430242004&q=London&aqi=no")
+    fetch(`https://api.weatherapi.com/v1/current.json?key=b1b0285f95c743d6b8862430242004&q=${city}&aqi=no`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+          setResults({
+            country: data.location.country,
+            cityName: data.location.name,
+            temperature: data.current.temp_c,
+            conditionText: data.current.condition.text,
+            icon: data.current.condition.icon
+          })
+        })
   }
 
   return (
@@ -19,7 +44,7 @@ const App = () => {
       {/* データ渡すとき・・・名前＝｛渡すもの} */}
       <Title />
       <Form setCity={setCity} getWeather={getWeather}/>
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,29 @@
-const Results = () => {
+type ResultsProps = {
+    results: {
+        country: string
+        cityName: string
+        temperature: string
+        conditionText: string
+        icon: string
+    }
+}
+
+const Results = (props: ResultsProps) => {
     return (
-        <h1>気象データ</h1>
+        <div>
+            {/* ロジカルオペレーター・・・条件が正しかったら右辺の内容を表示 */}
+            {props.results.country &&
+                <div>
+                    <div>{props.results.country}</div>
+                    <div>{props.results.cityName}</div>
+                    <div>{props.results.temperature}<span>℃</span>
+                    </div>
+                    <div>
+                        <img src={props.results.icon} alt="icon" />
+                        <span>{props.results.conditionText}</span>
+                    </div>
+                </div>}
+        </div>
     )
 }
 
